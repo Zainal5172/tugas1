7b6a81f3ade10883f3de6e4bdd2772a629059d49
Zainal5172 <128023464+Zainal5172@users.noreply.github.com>
2023-09-21 10:19:05 +0700

    Add files via upload

---
+++ TugasTiga1/TugasTiga1/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TugasTiga1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNIM = new System.Windows.Forms.Label();
+            this.lblNama = new System.Windows.Forms.Label();
+            this.lblKelas = new System.Windows.Forms.Label();
+            this.btnTampil = new System.Windows.Forms.Button();
+            this.btnKosong = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNIM
+            // 
+            this.lblNIM.AutoSize = true;
+            this.lblNIM.Location = new System.Drawing.Point(67, 71);
+            this.lblNIM.Name = "lblNIM";
+            this.lblNIM.Size = new System.Drawing.Size(39, 13);
+            this.lblNIM.TabIndex = 0;
+            this.lblNIM.Text = "NIM   :";
+            // 
+            // lblNama
+            // 
+            this.lblNama.AutoSize = true;
+            this.lblNama.Location = new System.Drawing.Point(67, 98);
+            this.lblNama.Name = "lblNama";
+            this.lblNama.Size = new System.Drawing.Size(44, 13);
+            this.lblNama.TabIndex = 0;
+            this.lblNama.Text = "Nama : ";
+            // 
+            // lblKelas
+            // 
+            this.lblKelas.AutoSize = true;
+            this.lblKelas.Location = new System.Drawing.Point(67, 128);
+            this.lblKelas.Name = "lblKelas";
+            this.lblKelas.Size = new System.Drawing.Size(39, 13);
+            this.lblKelas.TabIndex = 0;
+            this.lblKelas.Text = "Kelas :";
+            // 
+            // btnTampil
+            // 
+            this.btnTampil.Location = new System.Drawing.Point(60, 170);
+            this.btnTampil.Name = "btnTampil";
+            this.btnTampil.Size = new System.Drawing.Size(75, 23);
+            this.btnTampil.TabIndex = 1;
+            this.btnTampil.Text = "Tampilkan";
+            this.btnTampil.UseVisualStyleBackColor = true;
+            this.btnTampil.Click += new System.EventHandler(this.btnTampil_Click);
+            // 
+            // btnKosong
+            // 
+            this.btnKosong.Location = new System.Drawing.Point(185, 170);
+            this.btnKosong.Name = "btnKosong";
+            this.btnKosong.Size = new System.Drawing.Size(75, 23);
+            this.btnKosong.TabIndex = 1;
+            this.btnKosong.Text = "Kosongkan";
+            this.btnKosong.UseVisualStyleBackColor = true;
+            this.btnKosong.Click += new System.EventHandler(this.btnKosong_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKosong);
+            this.Controls.Add(this.btnTampil);
+            this.Controls.Add(this.lblKelas);
+            this.Controls.Add(this.lblNama);
+            this.Controls.Add(this.lblNIM);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNIM;
+        private System.Windows.Forms.Label lblNama;
+        private System.Windows.Forms.Label lblKelas;
+        private System.Windows.Forms.Button btnTampil;
+        private System.Windows.Forms.Button btnKosong;
+    }
+}
+
---
+++ TugasTiga1/TugasTiga1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TugasTiga1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTampil_Click(object sender, EventArgs e)
+        {
+            lblNIM.Text   ="NIM  : 22.11.5172";
+            lblNama.Text  ="Nama : M. Zainal Abidin";
+            lblKelas.Text ="Kelas : S1-22IF10";
+        }
+
+        private void btnKosong_Click(object sender, EventArgs e)
+        {
+            lblNIM.Text = "NIM  : ";
+            lblNama.Text = "Nama : ";
+            lblKelas.Text = "Kelas : ";
+        }
+    }
+}
